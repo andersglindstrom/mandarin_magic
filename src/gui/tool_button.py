a567ef3dc21ba0a87ca8a36da8e8e1662eea1e5f
andersglindstrom <anders@anderslindstrom.com>
2014-04-20 18:44:24 +0800

    Renamed Menu to ToolButton and renamed file accordingly.

---
+++ src/gui/tool_button.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+class ToolButton(QtGui.QPushButton):
+
+    decompose = QtCore.pyqtSignal()
+
+    def __init__(self, parent):
+        super(ToolButton, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        self.configureButton()
+        self.createContextMenu()
+        self.show()
+
+    def configureButton(self):
+        self.setText('Z')
+        self.setFixedHeight(20)
+        self.setFixedWidth(20)
+        self.setCheckable(True)
+        self.clicked.connect(self.buttonClicked)
+
+    def createContextMenu(self):
+        self._contextMenu = QtGui.QMenu(self)
+        action = self._contextMenu.addAction("Decompose")
+        action.triggered.connect(self.emit_decompose)
+
+    def emit_decompose(self):
+        self.decompose.emit()
+
+    def buttonClicked(self):
+        self._contextMenu.exec_(QtGui.QCursor.pos())
+
+class MenuDemo(QtGui.QWidget):
+
+    def __init__(self):
+        super(MenuDemo, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        menu = ToolButton(self)
+        menu.decompose.connect(self.decompose)
+        self.show()
+
+    def decompose(self):
+        print('Decompose')
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = MenuDemo()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
