76261f8e5156bb83eb3cad94930267b78198659e
andersglindstrom <anders@anderslindstrom.com>
2014-04-21 13:12:39 +0800

    Added comment about button styling.

--- src/mmagic/main.py
+++ src/mmagic/main.py
@@ -6,6 +6,10 @@ def mmagic_add_editor_button(self):
     button = tool_button.ToolButton(self.widget)
     self.iconsBox.addWidget(button)
     button.setFocusPolicy(QtCore.Qt.NoFocus)
+    # This should copy code from Anki editor.py
+    # Otherwise, plastiqueStyle will be used at all times
+    # and so not match other styles.
+    # Maybe can get style from other editor buttons instead?
     button.setStyle(self.plastiqueStyle)
 
 addHook("setupEditorButtons", mmagic_add_editor_button)
